c96415f574bdc9151bc2e107c348dd314d1331f8
windgodvc <987329896@qq.com>
2016-02-26 15:14:26 +0800

    updateSet

---
+++ options.js
@@ -0,0 +1,81 @@
+
+
+function OperationData(){
+	self = this;
+
+
+	self.init = function(){
+			$(document).ready(function () {
+				$('#copy').click(function () {
+					self.copy();     	
+			      });
+
+
+				$('#import').click(function () {
+					self.import();     	
+			      });
+			});
+
+			self.readConfig();
+	};
+
+	self.readConfig = function(){
+		result = "{" + self.generate("Theme",localStorage['Theme']) + "," + 
+		self.generate("UserTheme",localStorage['UserTheme']) + "," + 
+		self.generate("ColorValue",localStorage['localStorage']) + "," + 
+		self.generate("UserFont",localStorage['UserFont']) + "," + 
+		self.generate("UserCSS",localStorage['UserCSS']) + "}"
+		;
+		document.getElementById("Config").value = result;
+		var btn = document.getElementById('copy');
+	    var clipboard = new Clipboard(btn);
+
+	    clipboard.on('success', function(e) {
+	        alert("复制成功!");
+	        delete clipboard;
+	    });
+
+	    clipboard.on('error', function(e) {
+	        alert("复制失败! Chrome 版本 内核不支持!");
+	        delete clipboard;
+	    });
+	}
+
+
+	self.copy = function(){
+	    
+
+	};
+
+
+	self.import = function(){
+		result = document.getElementById("Config").value;
+		obj = JSON.parse(result);
+		localStorage['Theme'] = obj.Theme;
+		localStorage['UserTheme'] = obj.UserTheme;
+		localStorage['ColorValue'] = obj.ColorValue;
+		localStorage['UserFont'] = obj.UserFont;
+		localStorage['UserCSS'] = obj.UserCSS;
+		//console.log(obj);
+		alert("导入成功!");
+	};
+
+
+	self.generate = function(key,value){
+		if(value == 'null' || value == undefined){
+			value = '';
+		}
+		result =  "\"";
+		result += key;
+		result += "\":";
+		result += "\"";
+		result += value;
+		result += "\"";
+		return result;
+	}
+
+	return self;
+};
+
+
+OperationData().init();
